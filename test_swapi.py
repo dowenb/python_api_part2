f58df2fb4e41228d44747b424d76d00124165231
Ben Dowen <33220797+dowenb@users.noreply.github.com>
2019-11-14 23:53:46 +0000

    add docstring

--- test_swapi.py
+++ test_swapi.py
@@ -1,10 +1,10 @@
-# test_SWAPI.py
-from swapi import People
+# test_swapi.py
+"""Part two of test automation for APIs wit Python"""
 from assertpy import assert_that
-
+from swapi import People
 
 def test_get_people():
-    # Tests that SWAPI either turns an OK status or returns a 500 status
+    """Tests that SWAPI either turns an OK status or returns a 500 status"""
     people = People.get_people()
     if people.ok:
         assert_that(people.status_code).is_equal_to(200)
